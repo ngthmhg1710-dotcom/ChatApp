7577a7ed9f8f9a07046840cde5465bd1f2845a9d
Nguyễn Thị Minh Hương <52300201@student.tdtu.edu.vn>
2026-03-08 07:02:38 +0000

    Update chat-app/server/src/models/user.model.js

--- chat-app/server/src/models/user.model.js
+++ chat-app/server/src/models/user.model.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     minlength: 3,
     maxlength: 30
   },
+
   email: {
     type: String,
     required: [true, 'Email is required'],
@@ -18,33 +19,57 @@ const userSchema = new mongoose.Schema({
     trim: true,
     match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
   },
+
   password: {
     type: String,
     required: [true, 'Password is required'],
     minlength: 6,
     select: false
   },
+
+  // ===== EMAIL VERIFICATION =====
+
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+
+  verificationToken: {
+    type: String
+  },
+
+  verificationExpire: {
+    type: Date
+  },
+
+  // ===== USER INFO =====
+
   avatar: {
     type: String,
     default: 'https://ui-avatars.com/api/?name=User&background=random'
   },
+
   bio: {
     type: String,
     maxlength: 200,
     default: ''
   },
+
   isOnline: {
     type: Boolean,
     default: false
   },
+
   lastSeen: {
     type: Date,
     default: Date.now
   },
+
   friends: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }],
+
   friendRequests: [{
     from: {
       type: mongoose.Schema.Types.ObjectId,
@@ -55,10 +80,12 @@ const userSchema = new mongoose.Schema({
       default: Date.now
     }
   }],
+
   blockedUsers: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }]
+
 }, {
   timestamps: true
 });
@@ -68,7 +95,7 @@ userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
     return next();
   }
-  
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
